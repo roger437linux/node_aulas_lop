0bf529fdb65f94295c39be744d0cdd35f29bf64b
ROGERIO DA SILVA ALCANTARA <sn1024493@educ123.sp.senai.br>
2025-04-24 11:24:13 -0300

    Add file moldura

--- funções/moldura.js
+++ funções/moldura.js
@@ -1,1 +1,19 @@
 // Desenha uma moldura em frase
+
+const readline = require('readline-sync');
+const ch = '█';
+
+function barra(qtde) {
+	for (let i=0 ; i<qtde+4 ; i++) {
+		process.stdout.write(ch)
+	}
+	console.log();
+}
+
+//-- Fora da função
+
+const frase = readline.question('\nFrase? ');
+
+barra(frase.length);
+console.log(`${ch} ${frase} ${ch}`);
+barra(frase.length);
